ac32bb2381f301d30036ec1bfe9f3d1863ea501e
jakubantoszek <jakubantoszek1@gmail.com>
2023-04-04 22:39:15 +0200

    Create Configure connection window

---
+++ ConfigureConnection.py
@@ -0,0 +1,35 @@
+import tkinter as tk
+from tkinter import filedialog
+from tkinter import *
+
+
+class ConfigureConnection:
+    def __init__(self):
+        self.entry = None
+        self.algorithm_menu = None
+        self.selected_algorithm = None
+        self.window = Tk()
+        self.window.title("Configure connection")
+        self.window.geometry('300x225')
+
+        self.message = tk.Label(self.window, text="Opis ale nie wiem co tu napisac")
+        self.message.pack()
+
+        self.generate_key_frame()
+
+    def generate_key_frame(self):
+        frame = tk.LabelFrame(self.window, text="Configure connection")
+
+        entry_label = tk.Label(frame, text="Address: ")
+        entry_label.pack()
+
+        self.entry = tk.Entry(frame)
+        self.entry.pack()
+
+        send_button = tk.Button(frame, text='Connect', command=self.connect)
+        send_button.pack()
+
+        frame.pack(padx=10, pady=10)
+
+    def connect(self):
+        self.window.destroy()
