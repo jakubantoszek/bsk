de7f9a258216b9caedbce557aa644fd623534635
jakubantoszek <jakubantoszek1@gmail.com>
2023-04-17 16:13:14 +0200

    Login and register to user account

--- Login.py
+++ Login.py
@@ -1,4 +1,7 @@
+import os.path
 import tkinter as tk
+import hashlib
+path = "H:\\Studia\\Semestr 6\\bsk_users"
 
 
 class Login:
@@ -72,7 +75,20 @@ class Login:
 
     def login(self, window_type):
         if window_type == 'Login':
-            print("Login")
+            user_dir = os.path.join(path, self.username_entry.get())
+            if self.username_entry.get().strip() == '' or self.password_entry.get().strip() == '':
+                print("Username or password is empty")
+            elif not os.path.exists(user_dir):
+                print("User does not exist")
+            else:
+                with open(os.path.join(user_dir, "local_key.txt"), 'r') as file:
+                    password = file.read()
+                    password = password.replace('\n', '')
+                    if hashlib.sha256(self.password_entry.get().encode()).hexdigest() == password:
+                        print("Correct password")
+                    else:
+                        print("Incorrect password")
+
             self.window.destroy()
         elif window_type == 'Register':
             self.window.destroy()
@@ -80,8 +96,23 @@ class Login:
 
     def register(self, window_type):
         if window_type == 'Register':
-            print("Register")
-            self.window.destroy()
+            if self.username_entry.get().strip() == '' or self.repeat_password_entry.get().strip() == '' or self.password_entry.get().strip() == '':
+                print("Fields can't be empty")
+            elif os.path.exists(os.path.join(path, self.username_entry.get())):
+                print("User exists")
+            else:
+                password_hash = hashlib.sha256(self.password_entry.get().encode()).hexdigest()
+                repeated_password_hash = hashlib.sha256(self.repeat_password_entry.get().encode()).hexdigest()
+
+                if password_hash == repeated_password_hash:
+                    user_dir = os.path.join(path, self.username_entry.get())
+                    os.mkdir(user_dir)
+                    with open(os.path.join(user_dir, "local_key.txt"), 'w') as file:
+                        file.write(password_hash)
+                else:
+                    print("Password are not the same")
+
+                self.window.destroy()
         elif window_type == 'Login':
             self.window.destroy()
             self.create_window('Register')
